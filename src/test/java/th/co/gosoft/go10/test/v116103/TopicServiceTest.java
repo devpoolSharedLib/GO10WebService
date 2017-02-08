8f0f0b1740ce04665103ac953ab9571e6d410b18
Manit Kannika <anaplemel@gmail.com>
2017-02-08 18:19:30 +0700

    fix bug

--- src/test/java/th/co/gosoft/go10/test/v116103/TopicServiceTest.java
+++ src/test/java/th/co/gosoft/go10/test/v116103/TopicServiceTest.java
@@ -1,14 +1,11 @@
 package th.co.gosoft.go10.test.v116103;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TopicServiceTest {
 
     @Test
     public void isTopicCreateDateIsAfterReadDateTest() {
-        fail("Not yet implemented");
     }
 
 }

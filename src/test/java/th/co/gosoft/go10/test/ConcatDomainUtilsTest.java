94e720f573fec6854bb7ea96d5b0a0a8134564c6
Jirapas Chiradechwiroj <Plooer@Jirapas-MacBook-Pro.local>
2017-09-12 15:21:28 +0700

    fix test case

--- src/test/java/th/co/gosoft/go10/test/ConcatDomainUtilsTest.java
+++ src/test/java/th/co/gosoft/go10/test/ConcatDomainUtilsTest.java
@@ -42,12 +42,20 @@ public class ConcatDomainUtilsTest {
         assertEquals("Three Image Here<br><br><img src=\"https://s3-ap-southeast-1.amazonaws.com/x68br7d28053/GO10/DI2EFC\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br><img src=\"https://s3-ap-southeast-1.amazonaws.com/x68br7d28053/GO10/2QEJ337YA\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br><img src=\"https://s3-ap-southeast-1.amazonaws.com/x68br7d28053/GO10/2QEJ337YA\" width=\"230\" height=\"408\" alt=\"insertImageUrl\">",  ConcatDomainUtils.concatDomainImagePath(topicModelList.get(3).getContent()));
     }
 
+    @Test
+    public void concatDomainVideoPathTest(){
+    	TopicModel topicModel = new TopicModel();
+    	topicModel.setContent("<source src=\"JZU50IQPKZXVDY9IITFJ9R8XAE733.mp4\"></a>");
+    	assertEquals("<source src=\"https://s3-ap-southeast-1.amazonaws.com/x68br7d28053/GO10/JZU50IQPKZXVDY9IITFJ9R8XAE733.mp4\"></a>",ConcatDomainUtils.concatDomainImagePath(topicModel.getContent()));
+    }
+    
     private List<TopicModel> createConcatDomainImageDataList() {
         List<TopicModel> resultList = new ArrayList<TopicModel>();
         resultList.add(new TopicModel("0 image", null, "No Image Here"));
         resultList.add(new TopicModel("1 image", null, "<img src=\"DI2EFC\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br>One Image Here"));
         resultList.add(new TopicModel("2 image", null, "<img src=\"DI2EFC\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br>Two Image Here<br><br><img src=\"2QEJ337YA\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br>"));
         resultList.add(new TopicModel("3 image", null, "Three Image Here<br><br><img src=\"DI2EFC\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br><img src=\"2QEJ337YA\" width=\"230\" height=\"408\" alt=\"insertImageUrl\"><br><br><img src=\"2QEJ337YA\" width=\"230\" height=\"408\" alt=\"insertImageUrl\">"));
+        resultList.add(new TopicModel("4 image", null, "<a href=\"IJFBU9E.mp4\" style=\"display: none ></a>"));
         return resultList;
     }
 }
